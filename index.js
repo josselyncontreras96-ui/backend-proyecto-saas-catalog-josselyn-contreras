33edbaf7df9963607bec1dc9229e0b314c90f0f8
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-06-16 14:10:55 +0200

    instalacion y configuracion  de mongoose

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./src/config/db.js";
+connectDB();
 import express from "express";
+
 const app = express();
+
+
 
 app.get("/", (req, res) => {
   res.send("Bienvenidos a la API de Stacly!");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
